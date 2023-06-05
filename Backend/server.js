63e466d5ba06926551ca298ca6f299bbb7cf23c6
Jarvis <himanshushourabh@gmail.com>
2023-06-06 03:25:35 +0530

    v1.0.1

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,68 @@
-const express =require('express')
-const dotenv =require('dotenv')
-const colors= require('colors')
-const morgan = require('morgan')
-const cors = require('cors')
-const connectDB = require('./config/db')
+// const express =require('express')
+// const dotenv =require('dotenv')
+// const colors= require('colors')
+// const morgan = require('morgan')
+// const cors = require('cors')
+// const connectDB = require('./config/db')
 
 //dotenv configuration
-dotenv.config()
+// dotenv.config()
 
-//mongodb connection
-connectDB()
-//rest object 
-//storing features of exrs in a variable to use 
-const app = express()
+// //mongodb connection
+// connectDB()
+// //rest object 
+// //storing features of exrs in a variable to use 
+// const app = express()
 
-//middlewares
-app.use(express.json())
-app.use(cors())
-app.use(morgan('dev'))
+// //middlewares
+// app.use(express.json())
+// app.use(cors())
+// app.use(morgan('dev'))
 
-app.use('/api/v1/test' , require('./routes/testRoutes'))
-app.use('/api/v1/auth', require('./routes/authRoutes'))
+// app.use('/api/v1/test' , require('./routes/testRoutes'))
+// app.use('/api/v1/auth', require('./routes/authRoutes'))
 
-app.use('/api/v1/inventory' , require('./routes/inventoryRoutes'))
-//port
+// app.use('/api/v1/inventory' , require('./routes/inventoryRoutes'))
+// //port
+// const PORT = process.env.PORT || 8080;
+
+// //LISTEN METHOD
+
+// app.listen(PORT, () =>{
+//     console.log(`Server running in ${process.env.DEV_MODE} mode on ${process.env.PORT} port`.bgBlue.white)
+// })
+
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// dotenv configuration
+dotenv.config();
+
+// mongodb connection
+connectDB();
+
+// rest object
+// storing features of exrs in a variable to use
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// API routes
+app.use('/api/v1/test', require('./routes/testRoutes'));
+app.use('/api/v1/auth', require('./routes/authRoutes'));
+app.use('/api/v1/inventory', require('./routes/inventoryRoutes'));
+
+// port
 const PORT = process.env.PORT || 8080;
 
-//LISTEN METHOD
-
-app.listen(PORT, () =>{
-    console.log(`Server running in ${process.env.DEV_MODE} mode on ${process.env.PORT} port`.bgBlue.white)
-})
+// LISTEN METHOD
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.DEV_MODE} mode on port ${PORT}`.bgBlue.white);
+});
